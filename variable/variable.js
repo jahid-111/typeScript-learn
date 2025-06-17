d6a039d69b080b01de984f8b1ad0db105bc599a1
jahid-111 <jahidjob5@outlook.com>
2025-06-17 05:32:14 +0300

    Add JavaScript and TypeScript files with greeting variable and console log

---
+++ variable/variable.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = "Hello, TypeScript!";
+var num = 6;
+greeting.toLocaleLowerCase();
+console.log(greeting);
---
+++ variable/variable.ts
@@ -0,0 +1,8 @@
+let greeting: string = "Hello, TypeScript!";
+
+let num = 6;
+
+greeting.toLocaleLowerCase();
+console.log(greeting);
+
+export { };
