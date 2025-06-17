bc14ebea587da32d7df46299eef8cffe115d2c60
jahid-111 <jahidjob5@outlook.com>
2025-06-17 06:00:21 +0300

    number-boolean-typeInference

--- variable/variable.js
+++ variable/variable.js
@@ -1,6 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greeting = "Hello, TypeScript!";
-var num = 6;
 greeting.toLocaleLowerCase();
 console.log(greeting);
+// Number variable
+var number = 10e231;
+number.toExponential(2);
+console.log(number);
+// Boolean variable
+var isLoggedIn = false;
+console.log(isLoggedIn);
--- variable/variable.ts
+++ variable/variable.ts
@@ -1,8 +1,21 @@
 let greeting: string = "Hello, TypeScript!";
 
-let num = 6;
 
 greeting.toLocaleLowerCase();
 console.log(greeting);
+
+
+// Number variable
+let number: number = 10e231;
+
+number.toExponential(2);
+console.log(number);
+
+// Boolean variable
+let isLoggedIn: boolean = false;
+console.log(isLoggedIn);
+
+
+
 
 export { };
