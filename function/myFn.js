215a840f391f916720f0b3d0dc043286ef6e83d9
jahid-111 <jahidjob5@outlook.com>
2025-06-20 01:25:31 +0300

    Refactor functions to improve type definitions and add examples for return types

--- function/myFn.js
+++ function/myFn.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function sum(number) {
     // number.toUpperCase() // This line will cause an error because `toUpperCase` is not a method of type `number`.
     return number + 10;
+    // return "hello"; // This line will cause an error because the return type is declared as `number`, but we are returning a string.
 }
 sum(5);
 function getUpper(str) {
@@ -20,3 +21,29 @@ var isLoginUser = function (name, email, password, isPaid) {
     return "User ".concat(name, " logged in with email ").concat(email, " and password: ").concat(password, ", isPaid: ").concat(isPaid);
 };
 isLoginUser("jahid", "jahidjob4@gmail.com", 12345678);
+// ----------  ISSUE FOR UNION TYPE ---------------
+// function getValue(myVal: number)  : string{
+/* function getValue(myVal: number)  : boolean{
+
+    if (myVal > 5) {
+        return true;
+    }
+    return "status: 200"
+}
+ */
+// Return Type 
+var getHello = function (greet) {
+    return "Hello ".concat(greet);
+};
+var hero = ["Mohammad", "Jahid", "Islam"];
+hero.map(function (name) {
+    return "Hello ".concat(name);
+});
+// Use Void To know more definition this Function
+function consoleError(errMsg) {
+    console.error(errMsg);
+}
+// Use Never , with function never returns a value
+function handleError(errMsg) {
+    throw new Error(errMsg);
+}
--- function/myFn.ts
+++ function/myFn.ts
@@ -1,8 +1,9 @@
 
 
-function sum(number: number) {
+function sum(number: number): number {
     // number.toUpperCase() // This line will cause an error because `toUpperCase` is not a method of type `number`.
     return number + 10;
+    // return "hello"; // This line will cause an error because the return type is declared as `number`, but we are returning a string.
 }
 
 sum(5)
@@ -34,5 +35,49 @@ const isLoginUser = (name: string, email: string, password: number, isPaid: bool
 
 
 isLoginUser("jahid", "jahidjob4@gmail.com", 12345678)
+
+
+
+// ----------  ISSUE FOR UNION TYPE ---------------
+
+// function getValue(myVal: number)  : string{
+/* function getValue(myVal: number)  : boolean{
+
+    if (myVal > 5) {
+        return true;
+    }
+    return "status: 200"
+}
+ */
+
+
+
+// Return Type 
+const getHello = (greet: string): string => {
+    return `Hello ${greet}`;
+}
+
+
+
+const hero = ["Mohammad", "Jahid", "Islam"]
+
+
+hero.map((name): string => {
+    return `Hello ${name}`;
+})
+
+
+
+// Use Void To know more definition this Function
+function consoleError(errMsg: string): void {
+    console.error(errMsg);
+}
+
+// Use Never , with function never returns a value
+function handleError(errMsg: string): never {
+    throw new Error(errMsg);
+}
+
+
 
 export { }; 
