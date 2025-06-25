6d8387a7b97ff6729ac736fa87f542b68a4bdf19
jahid-111 <jahidjob5@outlook.com>
2025-06-25 09:06:54 +0300

    Readonly and Optional useCases

--- object/myObj.js
+++ object/myObj.js
@@ -21,3 +21,17 @@ function createUser2(user) {
     return user;
 }
 createUser2({ name: "", email: "", age: 0, isActive: true });
+var books = {
+    _id: "1",
+    title: "The Great Gatsby",
+    year: 1923,
+    author: "F. Scott Fitzgerald",
+    isPublished: true,
+    genres: ["Fiction", "Classic"]
+};
+books.title = "The Great Gatsby Updated";
+books.year = 1926;
+// books._id = "2"; // This will cause an error because _id is readonly
+function createBook(book) {
+    return book;
+}
--- object/myObj.ts
+++ object/myObj.ts
@@ -42,5 +42,34 @@ createUser2({ name: "", email: "", age: 0, isActive: true });
 
 
 
+type Book = {
+    readonly _id: string; // readonly means it cannot be changed after initialization (MongoDB _id)
+    // _id: string; // If you want to allow changes, remove readonly
+    title: string;
+    year: number;
+    author: string;
+    isPublished: boolean;
+    genres: string[];
+    creditCard?: number
+}
+
+let books: Book = {
+    _id: "1",
+    title: "The Great Gatsby",
+    year: 1923,
+    author: "F. Scott Fitzgerald",
+    isPublished: true,
+    genres: ["Fiction", "Classic"]
+}
+
+
+books.title = "The Great Gatsby Updated";
+books.year = 1926;
+// books._id = "2"; // This will cause an error because _id is readonly
+
+function createBook(book: Book): Book {
+    return book;
+}
+
 
 export { };
