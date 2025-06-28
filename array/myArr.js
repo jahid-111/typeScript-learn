8b08f10043945b9d1caf91c632af74ca174adbfc
jahid-111 <jahidjob5@outlook.com>
2025-06-28 09:24:07 +0300

    Add initial implementation of arrays and user type definitions

---
+++ array/myArr.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+var heroPowers = [];
+var power = [];
+superHeros.push("myMind");
+heroPowers.push(10);
+var allUsers = [];
+allUsers.push({ isActive: true, name: "John", age: 30 }); // This will cause an error if type checking is enabled
+console.log(allUsers);
+var MLModels = [
+    []
+];
---
+++ array/myArr.ts
@@ -0,0 +1,30 @@
+const superHeros: string[] = [];
+const heroPowers: number[] = [];
+
+const power: Array<number> = [];
+superHeros.push("myMind")
+heroPowers.push(10);
+
+
+type User = {
+    name: string;
+    age: number;
+    isActive: boolean;
+}
+
+
+const allUsers: User[] = []
+
+
+
+allUsers.push({ isActive: true, name: "John", age: 30 }); // This will cause an error if type checking is enabled
+
+console.log(allUsers);
+
+
+
+
+const MLModels: number[][] = [
+    []
+]
+export { }
