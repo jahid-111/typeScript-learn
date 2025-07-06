3c7804ed1e4c8eb9de538269b0fc433d72c85a32
jahid-111 <jahidjob5@outlook.com>
2025-07-06 08:58:46 +0300

    Knowing more about interface

--- interface/interface.js
+++ interface/interface.js
@@ -1,14 +1,26 @@
 "use strict";
+// TypeScript Interface Example
+// An interface is a way to define the structure of an object.
+// It can be used to define the shape of an object, including its properties and methods.
+// Interfaces are used to define the structure of an object, including its properties and methods.
 Object.defineProperty(exports, "__esModule", { value: true });
+// TypeScript Interface Example
+// An interface is a way to define the structure of an object.
+// It can be used to define the shape of an object, including its properties and methods.
+// Interfaces are used to define the structure of an object, including its properties and methods.
 var jahid = {
     db_Id: "121ceq",
     email: "jahidjob4@gmial.com",
     password: "email",
+    role: "Admin",
     signinBy: "Google",
     startTrail: function () { return "Start Trail to Redirect"; },
     firstMonth: function () { return "First Month"; },
     getCoupon: function (name) {
         return 10;
+    },
+    github: function () {
+        return "github.com/jahid-111";
     }
 };
 // jahid.age = 25; // This is allowed because age is optional
--- interface/interface.ts
+++ interface/interface.ts
@@ -17,6 +17,15 @@ interface User {
     getCoupon(couponName: string): number;
 
 }
+interface Admin extends User {
+    role: "Admin" | "User" | "SuperAdmin";
+}
+
+interface User {
+    github: () => string;
+    githubId?: string;
+}
+
 
 // TypeScript Interface Example
 // An interface is a way to define the structure of an object.
@@ -24,19 +33,27 @@ interface User {
 // Interfaces are used to define the structure of an object, including its properties and methods.
 
 
-
-
-const jahid: User = {
+const jahid: Admin | User = {
     db_Id: "121ceq",
     email: "jahidjob4@gmial.com",
     password: "email",
+    role: "Admin",
     signinBy: "Google",
     startTrail: () => { return "Start Trail to Redirect" },
     firstMonth: () => { return "First Month" },
     getCoupon: (name: "jahid10") => {
         return 10;
+    },
+    github: () => {
+        return "github.com/jahid-111";
     }
 };
+
+
+
+
+
+
 // jahid.age = 25; // This is allowed because age is optional
 
 // jahid.db_Id = 123; // This is allowed because db_Id is not readonly in this case
