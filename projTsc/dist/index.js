b8f142347442a6181932ca1570178075a93676c2
jahid-111 <jahidjob5@outlook.com>
2025-07-10 09:15:06 +0300

    public private syntx

--- projTsc/dist/index.js
+++ projTsc/dist/index.js
@@ -11,4 +11,14 @@ class User {
 }
 const jahid = new User("jahidjob4@gmail.com", "Jahid", 1234);
 jahid.city = "Dhaka";
-// jahid.email= ""  This will cause an error because pass is not readonly
+jahid.email;
+class Developer {
+    constructor(email, pass, isSenior) {
+        this.email = email;
+        this.pass = pass;
+        this.email = email;
+        this.pass = pass;
+        this.isSenior = isSenior;
+    }
+}
+const developerInfo = new Developer("jahidjob5@outlook.com", 123, true);
--- projTsc/src/index.ts
+++ projTsc/src/index.ts
@@ -18,4 +18,25 @@ class User {
 
 const jahid = new User("jahidjob4@gmail.com", "Jahid", 1234);
 jahid.city = "Dhaka";
-// jahid.email= ""  This will cause an error because pass is not readonly
+jahid.email
+
+
+
+
+class Developer {
+
+    isSenior: boolean
+
+    constructor(
+        public email: string,
+        private pass: number,
+        isSenior: boolean
+    ) {
+        this.email = email;
+        this.pass = pass;
+        this.isSenior = isSenior;
+    }
+}
+
+
+const developerInfo = new Developer("jahidjob5@outlook.com", 123, true);
